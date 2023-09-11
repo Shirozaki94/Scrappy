85dcd2ac1d6acdcfb237486138c9a0826b4fbf87
Shirozaki94 <129653770+Shirozaki94@users.noreply.github.com>
2023-09-11 16:18:33 +0300

    Updated .bat file
    Updated .bat file and removed unnecessary files.

--- start.bat
+++ start.bat
@@ -1,3 +1,3 @@
 @echo off
-cd /d "C:\Users\cioba\Documents\GitHub\Scrappy"
+cd /d "C:\Users\cioba\Documents\GitHub\Scrappy\Scrappy"
 "C:\Users\cioba\Documents\GitHub\Scrappy\venv\Scripts\python.exe" main.py
