ea82dccf0fa2b04855638e7262cf12fc9e401d81
Shirozaki94 <129653770+Shirozaki94@users.noreply.github.com>
2023-09-14 16:25:46 +0300

    New small update
    added interface drop down list

--- main.py
+++ main.py
@@ -12,54 +12,86 @@ class PacketAnalyzer:
     def __init__(self, root):
         self.root = root
         self.root.title("Packet Analyzer")
+
+        # Interface selection
+        self.interface_var = tk.StringVar()
+        self.interface_label = ttk.Label(root, text="Select Interface:")
+        self.interface_label.pack()
+        self.interface_dropdown = ttk.Combobox(root, textvariable=self.interface_var)
+        self.interface_dropdown.pack()
+        self.populate_interface_dropdown()
+
+        # Start button
         self.start_button = ttk.Button(root, text="Start", command=self.start)
         self.start_button.pack(pady=10)
+
+        # Stop button
         self.stop_button = ttk.Button(root, text="Stop", command=self.stop, state="disabled")
         self.stop_button.pack(pady=10)
-        self.stop_event = threading.Event()
+
+        # Save button
         self.save_button = ttk.Button(root, text="Save Info", command=self.save_info, state="disabled")
         self.save_button.pack(pady=10)
+
+        self.stop_event = threading.Event()
+
         self.fig = Figure(figsize=(6, 4), dpi=100)
         self.ax = self.fig.add_subplot(111)
         self.canvas = FigureCanvasTkAgg(self.fig, master=root)
         self.canvas.draw()
         self.canvas.get_tk_widget().pack(side=tk.TOP, fill=tk.BOTH, expand=True)
+
         self.top_ips_label = ttk.Label(root, text="Sending IPs:")
         self.top_ips_label.pack()
+
         self.top_ips_listbox = tk.Listbox(root, width=80, height=20)
         self.top_ips_listbox.pack()
+
         self.ip_packets = {}
         self.capture_running = False
         self.sniff_thread = None
+
+    def populate_interface_dropdown(self):
+        interfaces = get_windows_if_list()
+        interface_names = [iface['name'] for iface in interfaces]
+        self.interface_dropdown['values'] = interface_names
+        self.interface_dropdown.current(0)
 
     def start(self):
         self.start_button["state"] = "disabled"
         self.stop_button["state"] = "active"
         self.save_button["state"] = "disabled"
 
-        interfaces = get_windows_if_list()
-        print("Available Interfaces:")
-        for iface in interfaces:
-            print(f"Name: {iface['name']}, Description: {iface['description']}")
-
+        self.capture_running = True
         self.sniff_thread = threading.Thread(target=self.start_capture)
         self.sniff_thread.start()
 
     def start_capture(self):
-        self.capture_running = True
+        selected_interface = self.interface_var.get()
+        while self.capture_running and not self.stop_event.is_set():
+            sniff(prn=self.process_packet, iface=selected_interface, count=10)  # Process up to 10 packets per call
+
         def capture():
-            while self.capture_running:
-                if not self.stop_event.is_set():
-                    sniff(prn=self.process_packet, iface="Ethernet")
+            while self.capture_running and not self.stop_event.is_set():
+                sniff(prn=self.process_packet, iface=selected_interface, count=10)  # Process up to 10 packets per call
+
+        self.capture_running = True
         capture()
+
     def stop(self):
+        self.capture_running = False
         self.stop_event.set()
+        if self.sniff_thread:
+            self.sniff_thread.join()
         self.start_button["state"] = "active"
         self.stop_button["state"] = "disabled"
         self.save_button["state"] = "active"
-        self.stop_capture()
+
     def stop_capture(self):
         self.capture_running = False
+        if self.sniff_thread:
+            self.sniff_thread.join()
+
     def process_packet(self, pkt):
         if IP in pkt and pkt[IP].src == "192.168.1.80":
             src_ip = pkt[IP].src
@@ -69,13 +101,14 @@ class PacketAnalyzer:
                 src_port = pkt[TCP].sport if TCP in pkt else pkt[UDP].sport if UDP in pkt else None
                 if dst_port and src_port:
                     print(
-                        f'Source IP: {src_ip}, Destination IP: {dst_ip}, Destination Port: {dst_port}, Packets Sent: {pkt.summary()}')
+                        f'From IP: {src_ip}, Dest IP: {dst_ip}, Dest Port:{dst_port}, Packets Sent: {pkt.summary()}')
                     if dst_ip in self.ip_packets:
                         self.ip_packets[dst_ip].add((dst_port, pkt.summary()))
                     else:
                         self.ip_packets[dst_ip] = {(dst_port, pkt.summary())}
                     self.update_top_ips()
                     self.update_graph()
+
     def save_info(self):
         data = []
         for ip, info in self.ip_packets.items():
@@ -84,17 +117,21 @@ class PacketAnalyzer:
         df = pd.DataFrame(data, columns=["IP", "Port", "Packets"])
         df.to_excel("packet_info.xlsx", index=False)
         print("Info saved to packet_info.xlsx")
+
     def update_top_ips(self):
         self.top_ips_listbox.delete(0, tk.END)
         for ip, info in self.ip_packets.items():
             for port, packets in info:
                 self.top_ips_listbox.insert(tk.END, f"IP: {ip}, Port: {port}, Packets Sent: {packets}")
+
     def update_graph(self):
         self.ax.clear()
         ips = list(self.ip_packets.keys())
         port_counts = [len(info) for info in self.ip_packets.values()]
         self.ax.bar(ips, port_counts)
         self.canvas.draw()
+
+
 if __name__ == "__main__":
     root = tk.Tk()
     app = PacketAnalyzer(root)
